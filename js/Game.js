2c808f3662c52f8919a20028f8680418950ea4f9
Honeyfield <HoneyfieK@corning.com>
2020-05-08 22:49:28 -0400

    Phrase class is built, Game class is coming along, have both click and keyboard events handled, will need to look into disabling keyboard events while on the start screen

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,126 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor(){
+        this.missed = 0
+        this.phrases = [
+            {phrase: 'fencepost error'},
+            {phrase: 'This is Sparta'},
+            {phrase: 'Infinite Monkey Theorem'},
+            {phrase: 'Close But No Cigar'},
+            {phrase: 'Number five is alive'},
+        ]
+        this.activePhrase = null //to hold the string for the current active phrase
+        this.gamePhrase = null //to hold the active Phrase object for $this game
+    }
+
+    startGame(){
+        // hides the start screen overlay, calls the getRandomPhrase() method, 
+        // and sets the activePhrase property with the chosen phrase. 
+        // It also adds that phrase to the board by calling 
+        // the addPhraseToDisplay() method on the active Phrase object.
+        this.resetGame()
+        const overlay = document.getElementById('overlay')
+        overlay.classList.add('animated', 'flipOutX')
+        this.activePhrase = this.getRandomPhrase()
+        this.gamePhrase = new Phrase(this.activePhrase)
+        this.gamePhrase.addPhraseToDisplay()
+    } 
+
+    getRandomPhrase(){ 
+        // this method randomly retrieves one of the phrases stored 
+        // in the phrases array and returns it. 
+        const index = Math.floor(Math.random() * this.phrases.length)
+        return this.phrases[index].phrase
+    }
+    
+    handleInteraction(letter){
+        // this method controls most of the game logic. 
+        // It checks to see if the button clicked by the player matches a letter in the phrase, 
+        // and then directs the game based on a correct or incorrect guess. This method should:
+
+        // Disable the selected letter’s onscreen keyboard button.
+        // If the phrase does not include the guessed letter, 
+        //     add the wrong CSS class to the selected letter's keyboard button and call the removeLife() method.
+        // If the phrase includes the guessed letter, 
+        //     add the chosen CSS class to the selected letter's keyboard button, 
+        //     call the showMatchedLetter() method on the phrase, 
+        //     and then call the checkForWin() method. 
+        //     If the player has won the game, also call the gameOver() method.
+        const onScreenKeyboard = document.querySelectorAll('.key')
+        let selectedKey = null
+        onScreenKeyboard.forEach(key => { //figure out which key was press or selected
+            if(key.innerText === letter){
+                selectedKey = key //assign the DOM elem to the key
+            }
+        })
+        if( this.gamePhrase.checkLetter(letter) ){
+            selectedKey.classList.add('chosen', 'animated', 'bounceIn')
+            this.gamePhrase.showMatchedLetter(letter)
+        }
+        else{
+            selectedKey.classList.add('wrong', 'animated', 'wobble')
+            this.removeLife()
+        }
+    }
+
+    removeLife(){
+        // this method removes a life from the scoreboard, 
+        // by replacing one of the liveHeart.png images with a lostHeart.png image 
+        // (found in the images folder) and increments the missed property. 
+        // If the player has five missed guesses 
+        // (i.e they're out of lives), then end the game by calling the gameOver() method.
+        const tries = document.querySelectorAll('.tries')
+        if(this.missed < 4){
+            const index = this.missed
+            tries[index].classList.add('animated', 'hinge') //sad heart
+            setTimeout(() => { //a timeout to allow enough time for the animation to occur
+                tries[index].firstChild.src = 'images/lostHeart.png'
+                tries[index].classList.remove('animated', 'hinge')
+            }, 2000);
+            console.log(this.missed)
+            this.missed++
+        }
+        else{
+            this.gameOver()
+        }
+    }
+
+    checkForWin(){ 
+        // this method checks to see if the player has revealed all of the letters in the active phrase. 
+    }
+
+    gameOver(){ 
+        // this method displays the original start screen overlay, 
+        // and depending on the outcome of the game, 
+        // updates the overlay h1 element with a friendly win or loss message, 
+        // and replaces the overlay’s start CSS class with either the win or lose CSS class.
+        
+        // const overlay = document.getElementById('overlay')
+        // overlay.classList.remove('animated', 'flipOutX')
+        const ani = document.querySelectorAll('.animated')
+        ani.forEach(elem => {
+            elem.classList.remove('animated', 'flipOutX', 'hinge', 'hinge', 'wobble', 'bounceIn' )
+        })
+    }
+
+    resetGame(){
+        //clear the board and start fresh
+        const letters = document.querySelectorAll('.letter')
+        letters.forEach(item => {
+            item.remove()
+        })
+        const onScreenKeyboard = document.querySelectorAll('.key')
+        onScreenKeyboard.forEach(key => {
+            key.classList.remove('wrong', 'chosen')
+        })
+        const tries = document.querySelectorAll('.tries')
+        tries.forEach(item => {
+            item.firstChild.src = 'images/liveHeart.png'
+        })
+        
+        this.missed = 0
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,59 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Phrase {
+    constructor(phrase){
+        this.phrase = phrase.toLowerCase()
+    }
+
+    addPhraseToDisplay(){
+        // this adds letter placeholders to the display when the game starts. 
+        // Each letter is presented by an empty box, one li element for each letter. 
+        // See the example_phrase_html.txt file for an example of what the rendered 
+        // HTML for a phrase should look like when the game starts, 
+        // including any id or class attributes needed. When the player correctly 
+        // guesses a letter, the empty box is replaced with the matched letter 
+        // (see the showMatchedLetter() method below). 
+        // Make sure the phrase displayed on the screen uses the letter CSS class 
+        // for letters and the space CSS class for spaces.
+
+        const phaseUl = document.getElementById('phrase').getElementsByTagName('ul')[0]
+
+        const phraseArray = this.phrase.split('') //split the phrase into an array of letters and spaces 
+       
+        phraseArray.forEach(letter => { //create an li for each letter/space in the array then add it to the DOM
+            const elemLi = document.createElement('li')
+            elemLi.innerHTML = letter
+            if(/\s/.test(letter)){
+                elemLi.classList.add('space')
+            }
+            else{
+                elemLi.classList.add('letter', 'hide', letter)
+            }
+            phaseUl.append(elemLi)
+        })
+
+    }
+
+    checkLetter(test){
+        // checks to see if the letter selected by the player matches a letter in the phrase.
+        const letterMaybe = document.querySelectorAll(`.${test.toLowerCase()}`)
+        return letterMaybe.length > 0 ? true : false
+    }
+
+    showMatchedLetter(letter){
+        // reveals the letter(s) on the board that matches the player's selection. 
+        // To reveal the matching letter(s), select all of the letter DOM elements 
+        // that have a CSS class name that matches the selected letter and replace 
+        // each selected element's hide CSS class with the show CSS class.
+        const letterClass = document.querySelectorAll(`.${letter.toLowerCase()}`)
+        letterClass.forEach(item => {
+            item.classList.remove('hide')
+            item.classList.add('animated', 'rubberBand')
+            item.classList.add('show')
+        })
+        
+    }
+
+ }
